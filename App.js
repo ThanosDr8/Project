0e0d08fcb08121579ed40189372ab764df70ea5b
GEOVOUT06 <thanosdratzidis@gmail.com>
2025-11-24 09:44:34 +0200

    a

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.getElementById("menu-button").style.visibility= "hidden"
+  document.getElementById("menu-button").style.visibility = "hidden"
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.getElementById("menu-button").style.visibility= "visible"
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("menu-button").style.visibility = "visible"
 }
