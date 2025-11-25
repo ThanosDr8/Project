7b727db800cc5eddbd94883dd15e62032a8defda
Kyriakos <karandakik@gmail.com>
2025-11-25 11:22:15 +0200

    a

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -8,7 +8,7 @@ const taskSchema = new mongoose.Schema({
     enum: ["todo","in-progress","done"],
     default: "todo"
   },
-  priority: {type: String, enum: ["low","medium","high"],default:"meium" },
+  priority: {type: String, enum: ["low","medium","high"],default:"medium" },
   dueDate:Date,
   user:{type: mongoose.Schema.Types.ObjectID, ref:"User" },
   project: {type: mongoose.Schema.Types.ObjectID, ref:"Project" }
