0b0b5cf7dbda1a1c600c91484a32cfe07abddafa
GEOVOUT06 <thanosdratzidis@gmail.com>
2025-11-27 11:53:04 +0200

    a

--- info-help.js
+++ info-help.js
@@ -1,14 +1,8 @@
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("menu-button").style.display = "none"
-  if (window.innerWidth < 735) {
-  main.style.marginLeft = "0px";
-  }
-  else {
-  main.style.marginLeft = "250px";
-  }
+  document.getElementById("main").style.marginLeft = "250px";
 }
-
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
