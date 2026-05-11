acb260747696975254421fcda1646f7ef1475d2d
Kyriakos <karandakik@gmail.com>
2026-05-11 11:55:18 +0300

    έκανα κάποιες αλλαγές στον server  γιατί δεν άνοιγε σωστά

--- App.js
+++ App.js
@@ -205,6 +205,14 @@ function toggleDarkMode(isLight) {
     closeModal(modal);
     taskBeingEdited = null;
   }
+   function applyBorderColor(color) {
+    if (!color) return;
+    document.querySelectorAll(
+      "button, input, select, textarea, .modal-content, .filter-modal-content, .acc-modal-content, .settings-modal-content, .sidebar, .switch, .slider, hr, .task-card, .light-mode .modal-content, .light-mode .filter-modal-content, .light-mode .settings-modal-content, .light-mode .acc-modal-content, .task-name .light-input, light-input"
+    ).forEach(el => el.style.borderColor = color);
+    document.querySelectorAll(".switch .slider").forEach(s => s.style.backgroundColor = color);
+    localStorage.setItem("borderColor", color);
+  }
 
   // ======================
   // 2. Filters
